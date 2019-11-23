4c3a278bb9226fdaa5a7b4f07a6f4b052b011e7a
Arnaud Karambizi <arnaudkarambizi4@gmail.com>
2019-11-23 17:37:44 -0500

    made changes

--- server.js
+++ server.js
@@ -84,13 +84,22 @@ app.use('/products', products);
 //When deployed TO HEROKU OR PORT 5000
 // ────────────────────────────────────────────────────────────────────────────────
 
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('../client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(`../client/build/index.html`);
+//   });
+// }
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(`../client/build/index.html`);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
